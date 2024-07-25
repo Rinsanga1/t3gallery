c7ad14fbfcbeb79de6f996a3c54c2c7550cf56ac
rinsanga <raltelalrinsanga@mediamonks.com>
2024-07-25 13:49:12 +0000

    images fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 
 const mockUrls = [
-    https://utfs.io/f/84df573b-fe5f-44ed-b4e3-b88f87be0312-z3jdec.jfif,
-    https://utfs.io/f/734131f1-1d42-426b-8898-7144aadb5537-uofko.jpg,
-    https://utfs.io/f/1b7d8c16-9223-463c-ad63-a3917b0cbaad-gxepqe.jpg,
-    https://utfs.io/f/d5406e00-2b10-4cbf-93b1-9ba4b3bf8cc2-m1eaom.jpg
+    "https://utfs.io/f/84df573b-fe5f-44ed-b4e3-b88f87be0312-z3jdec.jfif",
+    "https://utfs.io/f/734131f1-1d42-426b-8898-7144aadb5537-uofko.jpg",
+    "https://utfs.io/f/1b7d8c16-9223-463c-ad63-a3917b0cbaad-gxepqe.jpg",
+    "https://utfs.io/f/d5406e00-2b10-4cbf-93b1-9ba4b3bf8cc2-m1eaom.jpg"
 ];
 
 const mockImages = mockUrls.map((url, index) => ({
@@ -17,8 +17,8 @@ export default function HomePage() {
     <main>
             <div className = "flex flex-wrap gap-4">{
                 mockImages.map((images) => (
-                <div key={image.id} className="w-48">
-                        <img src={image.url}/>
+                <div key={images.id} className="w-48">
+                        <img src={images.url}/>
                 </div>
                 ))}
             </div>
